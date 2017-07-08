85b751eb0bf818bd99444aa3f0d81ee13f2c8fc0
Andrew <flysurea@gmail.com>
2017-07-08 10:35:56 -0400

    fixed webpage issues

--- pageGenerator.py
+++ pageGenerator.py
@@ -1,4 +1,4 @@
-bluegill = {'id':'Bluegill','weight':'.5-1.5 lb.','length':'6-10 in.','comname':'bluegill,sunfish','sciname':'Lepomis macrochirus','depth':'1-2 ft.','lifespan':'5-6 yrs',
+bluegill = {'id':'bluegill','weight':'.5-1.5 lb.','length':'6-10 in.','comname':'bluegill,sunfish','sciname':'Lepomis macrochirus','depth':'1-2 ft.','lifespan':'5-6 yrs',
 'description':'The bluegill (Lepomis macrochirus) is a species of freshwater fish sometimes referred to as bream, brim, or copper nose. It is a member of the sunfish family Centrarchidae of the order Perciformes. It is native to North America and lives in streams, rivers, lakes, and ponds. It is commonly found east of the Rockies. It usually hides around, and inside, old tree stumps and other underwater structures. It can live in either deep or very shallow water, and will often move back and forth, depending on the time of day or season. Bluegills also like to find shelter among water plants and in the shade of trees along banks.Bluegills can grow up to 12 inches (30 cm) long and about  4 1⁄2 pounds. They have very distinctive coloring, with deep blue and purple on the face and gill cover, dark olive-colored bands down the side, and a fiery orange to yellow belly. The fish are omnivores and will eat anything they can fit in their mouth. They mostly feed on small aquatic insects.',
 'difficulty':'Easy','shoreboat':'shore','school':'10-20 fish','time':'none','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
 ,'environment':'near shores','pic':'.jpg' }
@@ -14,32 +14,53 @@ largemouth = {'id':'largemouth','weight':'~12 lbs.','length':'20-24 in.','comnam
 'description':'Adult largemouth bass are solitary fish, although occasionally several bass will congregate in areas with abundant food supplies. They are usually the apex predator in their habitat. They will hide between rocks, among water vegetation, or under roots and limbs of sunken trees, striking at their prey from the shadows. Bass prefer quiet, calm, and warm water but are very adaptable to other conditions. They are found in rivers, lakes, ponds, reservoirs, and steams.',
 'difficulty':'Medium','shoreboat':'shore','school':'10-20 fish','time':'springtime','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
 ,'environment':'clear, calm water','pic':'.jpg' }
-channelcat = {'id':'channelcat','weight':'2-4 lbs.','length':'8in-3 ft','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'5 yrs.','description':'',
-'difficulty':'','shoreboat':'','school':'','time':'','rarity':'common','hook':'large','aggressiveness':'no','takehome':''
-,'environment':'','pic':'.jpg' }
-shovelcat = {'id':'shovelcat','weight':'.5-1.5 lb.','length':'6-10 in.','comname':'bluegill,sunfish','sciname':'Lepomis macrochirus','depth':'1-2 ft.','lifespan':'5-6 yrs',
-'description':'The bluegill (Lepomis macrochirus) is a species of freshwater fish sometimes referred to as bream, brim, or copper nose. It is a member of the sunfish family Centrarchidae of the order Perciformes. It is native to North America and lives in streams, rivers, lakes, and ponds. It is commonly found east of the Rockies. It usually hides around, and inside, old tree stumps and other underwater structures. It can live in either deep or very shallow water, and will often move back and forth, depending on the time of day or season. Bluegills also like to find shelter among water plants and in the shade of trees along banks.Bluegills can grow up to 12 inches (30 cm) long and about  4 1⁄2 pounds. They have very distinctive coloring, with deep blue and purple on the face and gill cover, dark olive-colored bands down the side, and a fiery orange to yellow belly. The fish are omnivores and will eat anything they can fit in their mouth. They mostly feed on small aquatic insects.',
-'difficulty':'Easy','shoreboat':'shore','school':'10-20 fish','time':'none','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
-,'environment':'near shores','pic':'.jpg' }
-saugeye = {'id':'saugeye','weight':'1-1.5 lb.','length':'5-12 in.','comname':'smallie, red eye, green trout, brown bass, bronzeback, bareback bass','sciname':'Pomoxis annularis','depth':'1-5 ft.','lifespan':'4-6 yrs',
-'description':'Both species of crappie as adults feed predominantly on smaller species, including the young of their own predators (which include the northern pike, muskellunge, and walleye). They have diverse diets, however, including zooplankton, insects, and crustaceans.By day, crappie tend to be less active and will concentrate around weed beds or submerged objects, such as logs and boulders. They feed during dawn and dusk, by moving into open water or approaching the shore.',
-'difficulty':'Easy','shoreboat':'boat','school':'small schools, alline','time':'none','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
-,'environment':'near shores','pic':'.jpg' }
-muskie = {'id':'muskie','weight':'2-4 lbs.','length':'8in-3 ft','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'5 yrs.','description':'',
-'difficulty':'','shoreboat':'','school':'','time':'','rarity':'common','hook':'large','aggressiveness':'no','takehome':''
-,'environment':'','pic':'.jpg' }
-carp = {'id':'carp','weight':'2-4 lbs.','length':'8in-3 ft','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'5 yrs.','description':'',
-'difficulty':'','shoreboat':'','school':'','time':'','rarity':'common','hook':'large','aggressiveness':'no','takehome':''
-,'environment':'','pic':'.jpg' }
+bluecat = {'id':'BlueCatfish','weight':'20-40 lb.','length':'25-40 in.','comname':'blue','sciname':'Ictalurus furcatus','depth':'1-2 ft.','lifespan':'20-25 yrs.',
+'description':'The blue catfish (Ictalurus furcatus) is the largest species of North American catfish, reaching a length of 165 cm (65 in) and a weight of 68 kg (150 lb). The average length is about 25–46 in (64–117 cm). The fish can live to 20 years. The native distribution of blue catfish is primarily in the Mississippi River drainage, including the Missouri, Ohio, Tennessee, and Arkansas Rivers, and the Rio Grande, and south along the Gulf Coast to Belize and Guatemala. ',
+'difficulty':'Hard','shoreboat':'Boat','school':'none','time':'Dusk','rarity':'Rare','hook':'#6','aggressiveness':'Moderate','limit':'1 above 35 in.'
+,'environment':'low waters','pic':'.jpg' }
+perch = {'id':'perch','weight':'1 lb.','length':'1 ft.','comname':'Perch','sciname':'Perca fluviatilis','depth':'1-2 ft.','lifespan':'20-25 yrs.',
+'description':'The blue catfish (Ictalurus furcatus) is the largest species of North American catfish, reaching a length of 165 cm (65 in) and a weight of 68 kg (150 lb). The average length is about 25–46 in (64–117 cm). The fish can live to 20 years. The native distribution of blue catfish is primarily in the Mississippi River drainage, including the Missouri, Ohio, Tennessee, and Arkansas Rivers, and the Rio Grande, and south along the Gulf Coast to Belize and Guatemala. ',
+'difficulty':'Moderate','shoreboat':'Boat','school':'small schools','time':'Morning','rarity':'moderate-rare','hook':'#8','aggressiveness':'Moderate','limit':'None'
+,'environment':'mid-bottom water','pic':'.jpg' }
+rock = {'id':'rockbass','weight':'2-3 lbs.','length':'6-10 in.','comname':'rock perch, goggle-eye, red eye','sciname':'Ambloplites rupestris','depth':'surface-3ft','lifespan':'5-8 yrs.','description':'The rock bass (Ambloplites rupestris), also known as the rock perch, goggle-eye, red eye, is a fresh water fish native to east-central North America. This red eyed creature is a species of freshwater fish in the sunfish family (Centrarchidae) of order Perciformes and can be distinguished from other similar species by the six spines in the anal fin (other sunfish have only three anal fin spines).',
+'difficulty':'easy','shoreboat':'shore','school':'uncommon','time':'any','rarity':'common','hook':'#8','aggressiveness':'low','takehome':'no limit'
+,'environment':'Rocks','pic':'.jpg'}
+channelcatfish= {'id':'channel_catfish','weight':'3-40 lb.','length':'8-50 in.','comname':'Channel Catfish','sciname':'Ictalurus punctatus','depth':'Bottom Feeder','lifespan':'5-6 yrs',
+'description':'Channel catfish possess very keen senses of smell and taste. At the pits of their nostrils (nares) are very sensitive odor sensing organs with a very high concentration of olfactory receptors. In channel catfish, these organs are sensitive enough to detect several amino acids at about one part per 100 million in water.In addition, the channel catfish has taste buds distributed over the surface of its entire body.',
+'difficulty':'Easy','shoreboat':'Either','school':'None','time':'Night','rarity':'common','hook':'#2-#8','aggressiveness':'Little','limit':'(under 28 inches)None: 6 in a lake under 700 acres, and all other waters there is no quantity limit. Also no size limit anywhere(above 28 inches) limit 1 per day'
+,'environment':'Still Water','pic':'.jpg' }
+flatheadcatfish =  {'id':'flathead','weight':'3-50 lb.','length':'12-45 in.','comname':'Flathead Catfish','sciname':'Pylodictis olivaris','depth':'Bottom Feeders','lifespan':'5-6 yrs',
+'description':'Flatheads grow to a length of 155 cm (61 in) and may weigh up to 56 kg (123 lb), making it the second-largest North American catfish (after the blue catfish, Ictalurus furcatus). The average length is about 25-46 in (64-117 cm). Their maximum recorded lifespan is 24 years. Males are mature from 16 cm (6.3 in) and 4 years of age, while females mature from 18 cm (7.1 in) and 5 years of age, but may mature as late as 10 years. The world angling record flathead catfish was caught May 14, 1998, from Elk City Reservoir, Kansas, and weighed 123 lb 9 oz (56.0 kg)(Wikipedia)',
+'difficulty':'Easy','shoreboat':'Either','school':'No','time':'Night','rarity':'common','hook':'#2-#8','aggressiveness':'Medium','limit':'None for size or quantity'
+,'environment':'Still Water','pic':'.jpg' }
+muskellunge = {'id':'muskie','weight':'5-30 lb.','length':'24-60 in.','comname':'Muskellunge','sciname':'Esox masquinongy','depth':'surface-2 ft.','lifespan':'5-6 yrs',
+'description':'Muskellunge closely resemble other esocids such as the northern pike and American pickerel in both appearance and behavior. Like the northern pike and other aggressive pikes, the body plan is typical of ambush predators with an elongated body, flat head, and dorsal, pelvic and anal fins set far back on the body. Muskellunge are typically 28–48 in (71–122 cm) long and weigh 15–36 lb (6.8–16.3 kg),[1] though some have reached up to 6 ft (1.8 m) and almost 70 lb (32 kg).[2] According to past references the muskellunge attains 8 feet (244 cm) in length, this however has never been confirmed and is based most likely on exaggerations. A fish with a weight of 61.25 lb (27.8 kg) was caught in November 2000 in Georgian Bay, Ontario. The fish are a light silver, brown, or green, with dark vertical stripes on the flank, which may tend to break up into spots. In some cases, markings may be absent altogether, especially in fish from turbid waters. This is in contrast to northernpike',
+'difficulty':'Very Hard','shoreboat':'Either','school':'No','time':'Morning/Day','rarity':'Rare','hook':'medium to large sized frog or animal imatation lure','aggressiveness':'Very','limit':'No size limit but a 1 per day limit'
+,'environment':'near shores or moving water','pic':'.jpg' }
+carp = {'id':'carp','weight':'10lbs-50lbs lb.','length':'12in-50 in.','comname':'Carp','sciname':'Cyprinus carpio','depth':'Bottom Feeder','lifespan':'20 yrs',
+'description':'This fish is pretty much worthless.  It is not recommended to eat it and it also is not a fun fish to catch unless you just want to say that you caught a big fish.  This fish will not put up a fight it is more or less just a big blob that will eat anything in its path.',
+'difficulty':'Easy','shoreboat':'Either','school':'No','time':'none','rarity':'common','hook':'#2-#8','aggressiveness':'Little','limit':'None'
+,'environment':'Still Water','pic':'.jpg' }
+
+saugeye = {'id':'saugeye','weight':'2-5 lb.','length':'10in-24 in.','comname':'Saugeye, Sauger','sciname':'Sander canadensis','depth':'5-10 ft.','lifespan':'5-6 yrs',
+'description':'The sauger (Sander canadensis) is a freshwater perciform fish of the family Percidae which resembles its close relative the walleye. They are members of the largest vertebrate order, Perciformes.[2] They are the most migratory percid species in North America.[3] Saugers obtain two dorsal fins, the first is spiny and the posterior dorsal fin is a soft-rayed fin. Their paired fins are in the thoracic position and their caudal fin is truncated which means squared off at the corners, a characteristic of the Percidae family. Another physical characteristic of Saugers are their ctenoid scales which is common in advanced fishes. Saugers have a fusiform body structure, and as a result saugers are well adapted predatory fishes and are capable of swimming into fast currents with minimal drag on their bodies. They may be distinguished from walleyes by the distinctly spotted dorsal fin(Wikipedia)',
+'difficulty':'Moderate/Hard','shoreboat':'Either','school':'No','time':'Morning/Day','rarity':'Moderate','hook':'Usually caught with a medium sized spoon or spinner','aggressiveness':'Medium','limit':'6 per day with no size restriction'
+,'environment':'Open Water','pic':'.jpg' }
+
+spottedbass = {'id':'spotted','weight':'2lbs-6 lb.','length':'10in-24 in.','comname':'spotted/stripped bass','sciname':'Morone saxatilis','depth':'Surface to 7ft ft.','lifespan':'5-6 yrs',
+'description':'Many anglers who catch a spotted bass mistake it for a largemouth due to the coloration although there are subtle differences. The spotted bass, like all black basses except the largemouth, has scales on the base portion of the second dorsal fin, its first and second dorsal fin are clearly connected, and its upper jaw bone does not extend back to or beyond the rear edge of the eyes. The spotted bass is also often confused with a smallmouth bass or smally for short, but it lacks the vertical bars that are present on the sides of the body of a smallmouth bass. The spotted bass also has small black spots below the lateral line unlike either the large or smallmouth bass. Juveniles often resemble the young of smallmouth bass in having a broad band of orange at the base of the tail, followed by a broad black band and white edge. The spotted bass is known to hybridize with the smallmouth, which sometimes makes identification difficult.(Wikipedia)',
+'difficulty':'Moderate','shoreboat':'Either','school':'No','time':'Morning/Day','rarity':'common/moderate','hook':'#8-#14','aggressiveness':'Medium','limit':'5 fish that must be at least 12 inches in length'
+,'environment':'Open Water','pic':'.jpg' }
 
 
-array = [bluegill,crappie,smallmouth,largemouth,crappie]
+
+array = [bluegill,crappie,smallmouth,largemouth,bluecat,perch,rock,channelcatfish,flatheadcatfish,muskellunge,carp,saugeye,spottedbass]
 
 for fish in array:
     name = '/Users/AndrewsMacBookAir/Desktop/AlumCreekFishing/fish/' + str(fish['id']) + '.html'
     file = open(name,'w',encoding='utf-8')
     file.write(
-    """ <html><head><link rel='stylesheet' href='/Users/AndrewsMacBookAir/Desktop/AlumCreekFishing/css/bootstrap.css'>
+    """ <html><head><link rel='stylesheet' href='../css/bootstrap.css'>
       <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
       <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
       <meta charset='utf-8'>
