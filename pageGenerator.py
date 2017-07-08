7917a98ed42bee0174e74c133a1d8ebe579e610b
Andrew <flysurea@gmail.com>
2017-07-07 22:25:49 -0400

    styling changes

--- pageGenerator.py
+++ pageGenerator.py
@@ -6,16 +6,34 @@ crappie = {'id':'crappie','weight':'1-1.5 lb.','length':'5-12 in.','comname':'sm
 'description':'Both species of crappie as adults feed predominantly on smaller species, including the young of their own predators (which include the northern pike, muskellunge, and walleye). They have diverse diets, however, including zooplankton, insects, and crustaceans.By day, crappie tend to be less active and will concentrate around weed beds or submerged objects, such as logs and boulders. They feed during dawn and dusk, by moving into open water or approaching the shore.',
 'difficulty':'Easy','shoreboat':'boat','school':'small schools, alline','time':'none','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
 ,'environment':'near shores','pic':'.jpg' }
-smallmouth = {'weight':'2-4 lbs.','length': '','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'10-12 yrs.','description':'',
-'difficulty':'','shoreboat':'','school':'','time':'','rarity':'','hook':'','aggressiveness':'','takehome':''
+smallmouth = {'id':'smallmouth','weight':'.5-1.5 lb.','length':'15-20 in.','comname':'smallmouth, bronzeback, brown bass, brownie, smallie, bronze bass','sciname':'Micropterus dolomieu','depth':'1-2 ft.','lifespan':'10-12 yrs',
+'description':'The smallmouth bass is generally brown, appearing sometimes as black or green (seldom yellow) with red eyes, and dark brown vertical bands, rather than a horizontal band along the side. There are 13–15 soft rays in the dorsal fin. The upper jaw of smallmouth bass extends to the middle of the eye. The smallmouth\'s coloration and hue may vary according to environmental variables such as water clarity or diet.',
+'difficulty':'Medium','shoreboat':'shore','school':'10-20 fish','time':'springtime','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
+,'environment':'clear, calm water','pic':'.jpg' }
+largemouth = {'id':'largemouth','weight':'~12 lbs.','length':'20-24 in.','comname':'widemouth bass, bigmouth bass, black bass, bucketmouth, Potter\'s fish, Florida bass','sciname':'Micropterus salmoides','depth':'1-2 ft.','lifespan':'~16 yrs',
+'description':'Adult largemouth bass are solitary fish, although occasionally several bass will congregate in areas with abundant food supplies. They are usually the apex predator in their habitat. They will hide between rocks, among water vegetation, or under roots and limbs of sunken trees, striking at their prey from the shadows. Bass prefer quiet, calm, and warm water but are very adaptable to other conditions. They are found in rivers, lakes, ponds, reservoirs, and steams.',
+'difficulty':'Medium','shoreboat':'shore','school':'10-20 fish','time':'springtime','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
+,'environment':'clear, calm water','pic':'.jpg' }
+channelcat = {'id':'channelcat','weight':'2-4 lbs.','length':'8in-3 ft','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'5 yrs.','description':'',
+'difficulty':'','shoreboat':'','school':'','time':'','rarity':'common','hook':'large','aggressiveness':'no','takehome':''
 ,'environment':'','pic':'.jpg' }
-rockbass = {'weight':'','length': '','comname':'','sciname':'','depth':'','lifespan':'','description':'',
-'difficulty':'','shoreboat':'','school':'','time':'','rarity':'','hook':'','aggressiveness':'','takehome':''
+shovelcat = {'id':'shovelcat','weight':'.5-1.5 lb.','length':'6-10 in.','comname':'bluegill,sunfish','sciname':'Lepomis macrochirus','depth':'1-2 ft.','lifespan':'5-6 yrs',
+'description':'The bluegill (Lepomis macrochirus) is a species of freshwater fish sometimes referred to as bream, brim, or copper nose. It is a member of the sunfish family Centrarchidae of the order Perciformes. It is native to North America and lives in streams, rivers, lakes, and ponds. It is commonly found east of the Rockies. It usually hides around, and inside, old tree stumps and other underwater structures. It can live in either deep or very shallow water, and will often move back and forth, depending on the time of day or season. Bluegills also like to find shelter among water plants and in the shade of trees along banks.Bluegills can grow up to 12 inches (30 cm) long and about  4 1⁄2 pounds. They have very distinctive coloring, with deep blue and purple on the face and gill cover, dark olive-colored bands down the side, and a fiery orange to yellow belly. The fish are omnivores and will eat anything they can fit in their mouth. They mostly feed on small aquatic insects.',
+'difficulty':'Easy','shoreboat':'shore','school':'10-20 fish','time':'none','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
+,'environment':'near shores','pic':'.jpg' }
+saugeye = {'id':'saugeye','weight':'1-1.5 lb.','length':'5-12 in.','comname':'smallie, red eye, green trout, brown bass, bronzeback, bareback bass','sciname':'Pomoxis annularis','depth':'1-5 ft.','lifespan':'4-6 yrs',
+'description':'Both species of crappie as adults feed predominantly on smaller species, including the young of their own predators (which include the northern pike, muskellunge, and walleye). They have diverse diets, however, including zooplankton, insects, and crustaceans.By day, crappie tend to be less active and will concentrate around weed beds or submerged objects, such as logs and boulders. They feed during dawn and dusk, by moving into open water or approaching the shore.',
+'difficulty':'Easy','shoreboat':'boat','school':'small schools, alline','time':'none','rarity':'common','hook':'#8-#12','aggressiveness':'Medium','limit':'None'
+,'environment':'near shores','pic':'.jpg' }
+muskie = {'id':'muskie','weight':'2-4 lbs.','length':'8in-3 ft','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'5 yrs.','description':'',
+'difficulty':'','shoreboat':'','school':'','time':'','rarity':'common','hook':'large','aggressiveness':'no','takehome':''
 ,'environment':'','pic':'.jpg' }
-stripedbass = {'weight':'','length': '','comname':'','sciname':'','depth':'','lifespan':'','description':'',
-'difficulty':'','shoreboat':'','school':'','time':'','rarity':'','hook':'','aggressiveness':'','takehome':''
+carp = {'id':'carp','weight':'2-4 lbs.','length':'8in-3 ft','comname':'smallmouth bass','sciname':'Micropterus dolomieu','depth':'','lifespan':'5 yrs.','description':'',
+'difficulty':'','shoreboat':'','school':'','time':'','rarity':'common','hook':'large','aggressiveness':'no','takehome':''
 ,'environment':'','pic':'.jpg' }
-array = [bluegill,crappie]
+
+
+array = [bluegill,crappie,smallmouth,largemouth,crappie]
 
 for fish in array:
     name = '/Users/AndrewsMacBookAir/Desktop/AlumCreekFishing/fish/' + str(fish['id']) + '.html'
@@ -71,7 +89,6 @@ for fish in array:
                 <li class='list-group-item'>Rarity<span class='badge'>"""+ str(fish['rarity'])+ """</span></li>
                 <li class='list-group-item'>Hook size <span class='badge'>"""+ str(fish['hook'])+ """</span></li>
                 <li class='list-group-item'>Aggressiveness<span class='badge'>"""+ str(fish['aggressiveness'])+ """+</span></li>
-                <li class='list-group-item'>legal take-home size <span class='badge'>"""+ fish['limit']+ """</span></li>
                 <li class='list-group-item'>Water Environment <span class='badge'>"""+ fish['environment']+ """</span></li>
       </ul>
           </div>
